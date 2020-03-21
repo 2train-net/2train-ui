f1f4cf38d613459dba5bac0b7ba0de5e2c449d58
Anthony <anthony@firstfactory.com>
2020-03-21 15:24:08 -0600

    chore: register a service worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
+const TempComponent = () => <h1>Hello World</h1>;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Route exact path="/" component={TempComponent} />
+        <Route path="/completed" component={TempComponent} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
