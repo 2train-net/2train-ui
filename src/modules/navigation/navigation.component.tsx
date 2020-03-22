19805419247eee963c41d8a88d02c876d724a6ed
Anthony <anthony@firstfactory.com>
2020-03-21 18:34:13 -0600

    feat(navigation): display the navbar component into the navigation component

--- src/modules/navigation/navigation.component.tsx
+++ src/modules/navigation/navigation.component.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import Navbar from './components/navbar/navbar.component';
+
 const Navigation: FC = ({ children }) => (
   <Router>
+    <Navbar />
     <main>{children}</main>
   </Router>
 );
