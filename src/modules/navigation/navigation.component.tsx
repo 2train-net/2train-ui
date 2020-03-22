695bc752d94915a1eb4cdafd326f9eba3848d684
Anthony <anthony@firstfactory.com>
2020-03-21 18:50:38 -0600

    feat(navigation): add the different content layouts into the navigation component

--- src/modules/navigation/navigation.component.tsx
+++ src/modules/navigation/navigation.component.tsx
@@ -1,12 +1,21 @@
 import React, { FC } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import { Layout } from 'antd';
+
 import Navbar from './components/navbar/navbar.component';
+
+const { Header, Content, Footer } = Layout;
 
 const Navigation: FC = ({ children }) => (
   <Router>
-    <Navbar />
-    <main>{children}</main>
+    <Layout>
+      <Header>
+        <Navbar />
+      </Header>
+      <Content>{children}</Content>
+      <Footer>Footer</Footer>
+    </Layout>
   </Router>
 );
 
