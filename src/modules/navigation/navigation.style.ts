077844411991c802a2af3df31f1b1b442e3407ce
Anthony <anthony@firstfactory.com>
2020-04-08 21:00:24 -0600

    feat(navigation): add the sidebar and update the navbar

--- src/modules/navigation/navigation.style.ts
+++ src/modules/navigation/navigation.style.ts
@@ -2,13 +2,28 @@ import { createUseStyles } from 'react-jss';
 
 export default createUseStyles({
   root: {
+    minHeight: '100vh',
+    '& .logo': {
+      height: 32,
+      background: 'orange',
+      margin: 16
+    },
     '& header': {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
       padding: {
         right: 15,
         left: 15,
         '& span': {
           fontSize: 20
         }
+      }
+    },
+    '& main': {
+      padding: {
+        right: 15,
+        left: 15
       }
     }
   }
