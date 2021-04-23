642b69766557c3c8d59c8061d4f8577a104fed23
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-04-22 22:02:48 -0600

    feat(plans): add plan create component and create plan query

---
+++ src/modules/plans/plan-create/plan-create.component.tsx
@@ -0,0 +1,35 @@
+import React, { FC } from 'react';
+
+import { Card } from 'antd';
+
+import PlanForm from 'modules/plans/shared/components/plan-form/plan-form.component';
+
+import { IPlanFormValues } from 'modules/plans/shared/components/plan-form/plan-form.util';
+
+import FormHeader from 'shared/modules/form-header/form-header.component';
+
+import { useCreatePlanMutation } from 'shared/generated/graphql-schema';
+
+const PlanCreate: FC = () => {
+  const [createPlan] = useCreatePlanMutation();
+
+  const onSubmit = async (data: IPlanFormValues) => {
+    await createPlan({
+      variables: {
+        data
+      }
+    });
+  };
+
+  return (
+    <>
+      <FormHeader title="Create Plan" />
+      <br />
+      <Card>
+        <PlanForm onSubmit={onSubmit} />
+      </Card>
+    </>
+  );
+};
+
+export default PlanCreate;
