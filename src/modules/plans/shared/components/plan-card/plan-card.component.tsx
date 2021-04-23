607a2b4969789ace6895ee8deb52ef1703f5069f
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-04-22 22:01:48 -0600

    feat(plans): add plan card component

--- src/modules/plans/shared/components/plan-card/plan-card.component.tsx
+++ src/modules/plans/shared/components/plan-card/plan-card.component.tsx
@@ -1,34 +1,27 @@
 import React, { FC } from 'react';
 
-import { Card } from 'antd';
-import { DeleteOutlined, ShareAltOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons';
-
-import { IMasterComponent } from 'shared/modules/master-list/master-list.util';
-
 import { IPlanPayload } from 'modules/plans/shared/model';
+
+import Status from 'shared/modules/status/status.component';
+import ListCard from 'shared/modules/list-card/list-card.component';
+import { IMasterComponent } from 'shared/modules/master-list/master-list.util';
 
 interface IPlanCard extends IMasterComponent<IPlanPayload> {
   data: IPlanPayload;
 }
 
-const { Meta } = Card;
-
 const PlanCard: FC<IPlanCard> = ({ data }) => {
   return (
-    <Card
-      style={{ width: 300 }}
-      actions={[
-        <ShareAltOutlined key="share" />,
-        <DeleteOutlined key="delete" />,
-        <EditOutlined key="edit" />,
-        <EyeOutlined key="detail" />
-      ]}
-    >
-      <Meta
-        title={`${data.currency} ${data.price} | ${data.intervalCount} ${data.intervalPlan}`}
-        description={`${data.description} | ${data.scope} | ${data.status}`}
-      />
-    </Card>
+    <ListCard
+      uuid={data.uuid}
+      title={
+        <>
+          <Status status={data.status} />
+          <span style={{ marginLeft: 10 }}>{`${data.name}`}</span>
+        </>
+      }
+      description={`${data.currency} ${data.price} | ${data.intervalCount} ${data.intervalPlan}`}
+    />
   );
 };
 
