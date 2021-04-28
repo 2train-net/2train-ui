e97159845779dc454df34508a8b1e00effef3bc9
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-04-28 14:15:40 -0600

    feat(exercises): add exercises module

---
+++ src/modules/exercises/exercises.module.ts
@@ -0,0 +1,9 @@
+import Exercises from './exercises.component';
+
+import ExerciseList from './exercise-list/exercise-list.component';
+import ExerciseCreate from './exercise-create/exercise-create.component';
+import ExerciseUpdate from './exercise-update/exercise-update.component';
+
+export { ExerciseList, ExerciseCreate, ExerciseUpdate };
+
+export default Exercises;
