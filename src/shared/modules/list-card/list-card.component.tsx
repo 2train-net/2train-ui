db1aaf8dcf9978036b7fe83e1b837c1142ca7566
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-05-09 09:32:45 -0600

    feat(list-card): update list card component to be more generic

--- src/shared/modules/list-card/list-card.component.tsx
+++ src/shared/modules/list-card/list-card.component.tsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom';
 
 import { Card } from 'antd';
 import { DeleteOutlined, EditOutlined, EyeOutlined } from '@ant-design/icons';
+
 import { EDIT, DETAIL, DELETE } from 'shared/routes';
 
 interface IListCard {
@@ -13,11 +14,25 @@ interface IListCard {
   description: string | ReactNode;
   emptyActions?: boolean;
   actions?: ReactNode[];
+  leftContent?: ReactNode;
+  isViewActionEnabled?: boolean;
+  isEditActionEnabled?: boolean;
+  isDeleteActionEnabled?: boolean;
 }
 
 const { Meta } = Card;
 
-const ListCard: FC<IListCard> = ({ uuid, title, description, emptyActions = false, actions = [] }) => {
+const ListCard: FC<IListCard> = ({
+  uuid,
+  title,
+  description,
+  emptyActions = false,
+  isViewActionEnabled = true,
+  isEditActionEnabled = true,
+  isDeleteActionEnabled = true,
+  actions = [],
+  leftContent
+}) => {
   const location = useLocation();
   const history = useHistory();
 
@@ -27,17 +42,26 @@ const ListCard: FC<IListCard> = ({ uuid, title, description, emptyActions = fals
   const cardActions: ReactNode[] = actions;
 
   if (!emptyActions) {
-    cardActions.push(
-      <DeleteOutlined key="delete" onClick={() => redirect(`${pathname}/${DELETE}/${uuid}`)} />,
-      <EditOutlined key="edit" onClick={() => redirect(`${pathname}/${EDIT}/${uuid}`)} />,
-      <EyeOutlined key="detail" onClick={() => redirect(`${pathname}/${DETAIL}/${uuid}`)} />
-    );
+    if (isDeleteActionEnabled) {
+      cardActions.push(<DeleteOutlined key="delete" onClick={() => redirect(`${pathname}/${DELETE}/${uuid}`)} />);
+    }
+
+    if (isEditActionEnabled) {
+      cardActions.push(<EditOutlined key="edit" onClick={() => redirect(`${pathname}/${EDIT}/${uuid}`)} />);
+    }
+
+    if (isViewActionEnabled) {
+      cardActions.push(<EyeOutlined key="detail" onClick={() => redirect(`${pathname}/${DETAIL}/${uuid}`)} />);
+    }
   }
 
   return (
     <>
       <Card style={{ width: 300 }} actions={cardActions}>
-        <Meta title={title} description={description} />
+        <div style={{ display: 'flex' }}>
+          {leftContent && <div style={{ margin: 'auto', marginRight: 16 }}>{leftContent}</div>}
+          <Meta title={title} description={description} />
+        </div>
       </Card>
     </>
   );
