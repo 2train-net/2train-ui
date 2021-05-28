313fd35d0aadf28ec81fea5484c14fe7310b9abe
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-05-27 20:53:50 -0600

    feat(workout-routines): use private route instead of route

--- src/modules/workout-routines/workout-routines.component.tsx
+++ src/modules/workout-routines/workout-routines.component.tsx
@@ -3,12 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import { WorkoutRoutineUpdate } from 'modules/workout-routines/workout-routines.module';
 
+import { PrivateRoute } from 'shared/modules/route';
 import { WORKOUT_ROUTINE_EDIT } from 'shared/routes';
 import { NotFoundErrorPage } from 'shared/modules';
 
 const WorkoutRoutines: FC = () => (
   <Switch>
-    <Route exact path={WORKOUT_ROUTINE_EDIT} component={WorkoutRoutineUpdate} />
+    <PrivateRoute exact path={WORKOUT_ROUTINE_EDIT} component={WorkoutRoutineUpdate} />
 
     <Route component={NotFoundErrorPage} />
   </Switch>
