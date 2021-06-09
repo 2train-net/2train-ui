f7f1b4c5b3937ca50a104886b088055382c44357
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-06-08 18:55:06 -0600

    feat(training-day-list): add training day list component

---
+++ src/modules/training/training-day-list/training-day-list.component.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useContext } from 'react';
+
+import { useHistory } from 'react-router-dom';
+
+import { Card, Col, Typography } from 'antd';
+
+import _ from 'lodash';
+
+import { ListItem } from 'shared/modules';
+import { AuthContext } from 'shared/contexts';
+import { IWorkoutExercise } from 'shared/model';
+import { WorkoutRoutineService } from 'shared/services';
+
+const { parseNumberToDay, getMaxDay } = WorkoutRoutineService;
+
+const { Title } = Typography;
+
+const TrainingDayList: FC = () => {
+  const { user } = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const days = Array.from(Array(getMaxDay(user?.currentActivePlan?.workoutRoutine?.workoutExercises) + 1).keys());
+
+  const grouped = _.groupBy(user?.currentActivePlan?.workoutRoutine?.workoutExercises, 'day');
+
+  const calculateAmountOfWorkoutExercises = (workoutExercises: IWorkoutExercise[]) => {
+    return workoutExercises.reduce((acc, current) => {
+      if (!current.workoutId) acc++;
+      return acc;
+    }, 0);
+  };
+
+  return (
+    <Card style={{ height: '100%', marginTop: 10 }} bodyStyle={{ paddingLeft: 0, paddingRight: 0 }}>
+      <Title style={{ marginLeft: 15 }} level={5}>
+        Elegir día de entrenamiento:
+      </Title>
+
+      {days.map(day => {
+        return parseNumberToDay(day) in grouped ? (
+          <Col span={24} key={`list-item-${day}`}>
+            <ListItem
+              title={`Día ${day + 1}`}
+              description={`Cantidad de ejercicios: ${calculateAmountOfWorkoutExercises(
+                grouped[parseNumberToDay(day)]
+              )}`}
+              onDetail={() => history.push(`training/workout/${day}`)}
+            />
+          </Col>
+        ) : (
+          ''
+        );
+      })}
+    </Card>
+  );
+};
+
+export default TrainingDayList;
