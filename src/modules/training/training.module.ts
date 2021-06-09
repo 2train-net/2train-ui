347b630847bab6df396ef0804ddaf9b3fd724e07
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-06-08 18:56:39 -0600

    feat(training): add training module

---
+++ src/modules/training/training.module.ts
@@ -0,0 +1,13 @@
+import Training from './training.component';
+
+import TrainingDayList from 'modules/training/training-day-list/training-day-list.component';
+import TrainingWorkoutDetail from 'modules/training/training-workout-detail/training-workout-detail.component';
+import TrainingWorkoutExerciseList from 'modules/training/training-workout-exercise-list/training-workout-exercise-list.component';
+
+import TrainingWorkoutExerciseForm from 'modules/training/shared/components/training-workout-exercise-form/training-workout-exercise-form.component';
+
+export * from './shared/model';
+
+export { TrainingDayList, TrainingWorkoutExerciseList, TrainingWorkoutDetail, TrainingWorkoutExerciseForm };
+
+export default Training;
