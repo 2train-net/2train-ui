b11c70b91cc428c72ffdd1bb1e6fe7555c3e17d4
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-06-10 18:19:23 -0600

    feat(modal): add submit button available option

--- src/shared/model/modal.interface.ts
+++ src/shared/model/modal.interface.ts
@@ -14,6 +14,7 @@ export interface IModal {
   cancelText?: string;
   isLoading?: boolean;
   isCancelButtonAvailable?: boolean;
+  isSubmitButtonAvailable?: boolean;
   onConfirm?: () => any;
   onCancel?: () => any;
 }
