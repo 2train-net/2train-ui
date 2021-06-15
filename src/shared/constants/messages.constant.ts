a7a69516c8e02c4b48ec467bb5432a5c05d8b25c
Anthony <anthony@Anthonys-MacBook-Pro.local>
2021-06-15 17:30:28 -0600

    feat(service-worker): add refresh confirm modal for pwa cache reload

--- src/shared/constants/messages.constant.ts
+++ src/shared/constants/messages.constant.ts
@@ -87,4 +87,5 @@ export const REQUIRED_EXCEPTION_TEXT = 'Requerido',
 
 export const WRONG_CREDENTIALS_ERROR_TEXT = `${EMAIL_TEXT} ó ${PASSWORD_TEXT.toLowerCase()} incorrectos`;
 
-export const COPY_ON_CLIPBOARD_SUCCESSFULLY_TEXT = 'Copiado en el portapapeles correctamente';
+export const COPY_ON_CLIPBOARD_SUCCESSFULLY_TEXT = 'Copiado en el portapapeles correctamente',
+  NEW_APP_VERSION_TEXT = 'Hay una nueva versión, deseas actualizarla?';
