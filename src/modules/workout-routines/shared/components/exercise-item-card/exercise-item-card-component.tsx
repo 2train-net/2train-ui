c284a61f996f6dfc0d63c3fa4170f5bf5a5b028a
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-06-24 23:31:08 -0600

    feat(exercise-item-card): add none text when exercise doesnt have description

--- src/modules/workout-routines/shared/components/exercise-item-card/exercise-item-card-component.tsx
+++ src/modules/workout-routines/shared/components/exercise-item-card/exercise-item-card-component.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 
 import { IExercisePayload } from 'modules/exercises/shared/model/exercise-payload.interface';
 
+import { NONE_TEXT } from 'shared/constants';
 import ListCard from 'shared/modules/list-card/list-card.component';
 
 interface IExerciseCardValues {
@@ -9,7 +10,14 @@ interface IExerciseCardValues {
 }
 
 const ExerciseItemCard: FC<IExerciseCardValues> = ({ data }) => {
-  return <ListCard uuid={data.uuid} title={data.name} description={data.description} emptyActions={true} />;
+  return (
+    <ListCard
+      uuid={data.uuid}
+      title={data.name}
+      description={data.description ? data.description : NONE_TEXT}
+      emptyActions={true}
+    />
+  );
 };
 
 export default ExerciseItemCard;
