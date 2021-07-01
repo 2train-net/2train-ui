e6832ebab95bc328bdb0822141f1b2a068addd6e
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-06-30 19:47:48 -0600

    feat(theme): separate breackpoints from light theme

--- src/shared/theme/theme.ts
+++ src/shared/theme/theme.ts
@@ -1,3 +1,24 @@
+import { Breakpoint } from './breakpoint.interface';
+
 export const DEFAULT_SPACING = 8;
 
 export const spacing = (n: number) => n * DEFAULT_SPACING;
+
+export const breakpoints = {
+  up: (breakpoint: Breakpoint) =>
+    ({
+      xs: '@media (min-width: 0px)',
+      sm: '@media (min-width: 600px)',
+      md: '@media (min-width: 960px)',
+      lg: '@media (min-width: 1280px)',
+      xl: '@media (min-width: 1920px)'
+    }[breakpoint]),
+  down: (breakpoint: Breakpoint) =>
+    ({
+      xs: '@media (max-width: 599.98px)',
+      sm: '@media (max-width: 959.98px)',
+      md: '@media (max-width: 1279.98px)',
+      lg: '@media (max-width: 1919.98px)',
+      xl: '@media (max-width: 2559.98px)'
+    }[breakpoint])
+};
