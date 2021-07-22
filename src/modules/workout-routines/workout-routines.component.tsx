c6250837d9a1123fb652fd5c0305b97c0bb3b915
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-07-22 00:08:17 -0600

    feat(workout-routines): use new routes with the new features added

--- src/modules/workout-routines/workout-routines.component.tsx
+++ src/modules/workout-routines/workout-routines.component.tsx
@@ -1,16 +1,34 @@
 import React, { FC } from 'react';
 import { Redirect, Switch } from 'react-router-dom';
 
-import { WorkoutRoutineUpdate, WorkoutRoutineDetail } from 'modules/workout-routines/workout-routines.module';
+import {
+  WorkoutRoutineUpdate,
+  WorkoutRoutineDetail,
+  WorkoutRoutineList,
+  WorkoutRoutineCreate
+} from 'modules/workout-routines/workout-routines.module';
 
 import { PrivateRoute } from 'shared/modules/route';
-import { NOT_FOUND, WORKOUT_ROUTINE_DETAIL, WORKOUT_ROUTINE_EDIT } from 'shared/routes';
+import {
+  NOT_FOUND,
+  WORKOUT_ROUTINES,
+  WORKOUT_ROUTINE_ADD,
+  WORKOUT_ROUTINE_DELETE,
+  WORKOUT_ROUTINE_DETAIL,
+  WORKOUT_ROUTINE_EDIT
+} from 'shared/routes';
 
 const WorkoutRoutines: FC = () => (
   <Switch>
+    <PrivateRoute exact path={WORKOUT_ROUTINE_ADD} component={WorkoutRoutineCreate} />
+
     <PrivateRoute exact path={WORKOUT_ROUTINE_EDIT} component={WorkoutRoutineUpdate} />
 
     <PrivateRoute exact path={WORKOUT_ROUTINE_DETAIL} component={WorkoutRoutineDetail} />
+
+    <PrivateRoute exact path={WORKOUT_ROUTINE_DELETE} component={WorkoutRoutineList} />
+
+    <PrivateRoute exact path={WORKOUT_ROUTINES} component={WorkoutRoutineList} />
 
     <Redirect to={NOT_FOUND} />
   </Switch>
