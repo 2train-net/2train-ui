c1e90fbfc6035da126ed2f8ffa1782b74df43f17
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-07-22 00:06:53 -0600

    feat(workout-routines): add workout routine list component

---
+++ src/modules/workout-routines/workout-routine-list/workout-routine-list.component.tsx
@@ -0,0 +1,57 @@
+import React, { FC, useContext, useEffect } from 'react';
+
+import { useHistory, useLocation } from 'react-router';
+
+import {
+  WorkoutRoutineCard,
+  IWorkoutRoutinePayload,
+  SINGULAR_ROUTINE_TITLE,
+  PLURAL_ROUTINE_TITLE
+} from 'modules/workout-routines/workout-routines.module';
+
+import { MasterList } from 'shared/modules';
+import { AuthContext, ModalContext } from 'shared/contexts';
+import { DELETE_MODAL } from 'shared/constants';
+import { DELETE, WORKOUT_ROUTINES } from 'shared/routes';
+import { useGetWorkoutRoutinesQuery, UserType } from 'shared/generated';
+
+const ExerciseList: FC = () => {
+  const history = useHistory();
+  const location = useLocation();
+
+  const { user } = useContext(AuthContext);
+  const modalProvider = useContext(ModalContext);
+
+  const isPersonaTrainer = user?.type === UserType.PersonalTrainer;
+
+  const redirectToWorkoutRoutines = () => {
+    history.push(WORKOUT_ROUTINES);
+  };
+
+  const deleteWorkoutRoutine = () => {};
+
+  const displayDeleteConfirmation = () => {
+    modalProvider.show({
+      ...DELETE_MODAL,
+      onConfirm: deleteWorkoutRoutine,
+      onCancel: redirectToWorkoutRoutines
+    });
+  };
+
+  useEffect(() => {
+    if (location.pathname.match(DELETE)) {
+      displayDeleteConfirmation();
+    }
+  }, [location]);
+
+  return (
+    <MasterList<IWorkoutRoutinePayload>
+      title={[SINGULAR_ROUTINE_TITLE, PLURAL_ROUTINE_TITLE]}
+      render={WorkoutRoutineCard}
+      isCreateButtonAvailable={isPersonaTrainer}
+      useQuery={useGetWorkoutRoutinesQuery}
+    />
+  );
+};
+
+export default ExerciseList;
