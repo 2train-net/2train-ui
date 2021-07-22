f8e4c3a6e7b9d1decae7ba08f24db02c28fb34bd
ITR20-08 <isaac.toumarodriguez@gmail.com>
2021-07-22 00:09:03 -0600

    feat(workout-routines): update workout routines module

--- src/modules/workout-routines/workout-routines.module.ts
+++ src/modules/workout-routines/workout-routines.module.ts
@@ -1,9 +1,13 @@
 import WorkoutRoutines from './workout-routines.component';
 
+import WorkoutRoutineList from './workout-routine-list/workout-routine-list.component';
+import WorkoutRoutineCreate from './workout-routine-create/workout-routine-create.component';
 import WorkoutRoutineUpdate from './workout-routine-update/workout-routine-update.component';
 import WorkoutRoutineDetail from './workout-routine-detail/workout-routine-detail.component';
 
 import ExerciseItemCard from './shared/components/exercise-item-card/exercise-item-card-component';
+import WorkoutRoutineCard from './shared/components/workout-routine-card/workout-routine-card.component';
+import WorkoutRoutineForm from './shared/components/workout-routine-form/workout-routine-form.component';
 import WorkoutExerciseForm from './shared/components/workout-exercise-form/workout-exercise-form.component';
 import ExerciseOptionCreate from './shared/components/exercise-option-create/exercise-option-create.component';
 import WorkoutExerciseDetail from './shared/components/workout-exercise-detail/workout-exercise-detail.component';
@@ -16,10 +20,14 @@ export * from './shared/components/workout-exercise-form/workout-exercise-form.u
 export * from './workout-routine-update/workout-routine-update.util';
 
 export {
+  WorkoutRoutineCreate,
+  WorkoutRoutineList,
   WorkoutRoutineUpdate,
   WorkoutRoutineDetail,
   ExerciseOptionCreate,
   ExerciseItemCard,
+  WorkoutRoutineCard,
+  WorkoutRoutineForm,
   WorkoutExerciseItemCard,
   WorkoutExerciseForm,
   WorkoutExerciseDetail
