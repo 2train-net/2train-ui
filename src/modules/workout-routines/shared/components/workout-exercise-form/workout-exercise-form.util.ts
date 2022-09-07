697968fa68b4fdc5a691107de27f2363c7879f15
2train-alessandro <alessandro@2train.net>
2022-09-07 10:13:37 -0600

    feat(workout-routines): add validation to sets,reps and seconds in workout-exercise-form

--- src/modules/workout-routines/shared/components/workout-exercise-form/workout-exercise-form.util.ts
+++ src/modules/workout-routines/shared/components/workout-exercise-form/workout-exercise-form.util.ts
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-import { NUMBER_TYPE_EXCEPTION_TEXT, REQUIRED_EXCEPTION_TEXT } from 'shared/constants';
+import { NUMBER_TYPE_EXCEPTION_TEXT, REQUIRED_EXCEPTION_TEXT, NUMBER_MORE_THAN_EXCEPTION_TEXT } from 'shared/constants';
 import { UnitMeasure } from 'shared/generated';
 
 export enum WorkoutExerciseFocus {
@@ -22,19 +22,28 @@ export const WORKOUT_EXERCISE_FORM_SCHEMA = Yup.object().shape({
     .oneOf([WorkoutExerciseFocus.REPS, WorkoutExerciseFocus.SPRINT])
     .required(REQUIRED_EXCEPTION_TEXT),
 
-  sets: Yup.number().typeError(NUMBER_TYPE_EXCEPTION_TEXT).required(REQUIRED_EXCEPTION_TEXT),
+  sets: Yup.number()
+    .typeError(NUMBER_TYPE_EXCEPTION_TEXT)
+    .required(REQUIRED_EXCEPTION_TEXT)
+    .moreThan(0, NUMBER_MORE_THAN_EXCEPTION_TEXT),
   reps: Yup.number()
     .typeError(NUMBER_TYPE_EXCEPTION_TEXT)
     .when('focus', {
       is: WorkoutExerciseFocus.REPS,
-      then: Yup.number().typeError(NUMBER_TYPE_EXCEPTION_TEXT).required(REQUIRED_EXCEPTION_TEXT),
+      then: Yup.number()
+        .typeError(NUMBER_TYPE_EXCEPTION_TEXT)
+        .required(REQUIRED_EXCEPTION_TEXT)
+        .moreThan(0, NUMBER_MORE_THAN_EXCEPTION_TEXT),
       otherwise: Yup.number().typeError(NUMBER_TYPE_EXCEPTION_TEXT).nullable().default(null),
     }),
   seconds: Yup.number()
     .typeError(NUMBER_TYPE_EXCEPTION_TEXT)
     .when('focus', {
       is: WorkoutExerciseFocus.SPRINT,
-      then: Yup.number().typeError(NUMBER_TYPE_EXCEPTION_TEXT).required(REQUIRED_EXCEPTION_TEXT),
+      then: Yup.number()
+        .typeError(NUMBER_TYPE_EXCEPTION_TEXT)
+        .required(REQUIRED_EXCEPTION_TEXT)
+        .moreThan(0, NUMBER_MORE_THAN_EXCEPTION_TEXT),
       otherwise: Yup.number().typeError(NUMBER_TYPE_EXCEPTION_TEXT).nullable().default(null),
     }),
   weight: Yup.number().required(REQUIRED_EXCEPTION_TEXT),
